478d35b7e4da768f8968ac590530fefd931b6d3b
Ethan <ethan.steip@gmail.com>
2023-01-03 12:10:52 -0500

    fixing view all products route

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const viewAllProductsRoutes = require('./routes/view-all-products.js');
 
 // Mounet all resourc routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +39,7 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/view-all-products', viewAllProductsRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -46,11 +48,6 @@ app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-
-app.get('/view-all-products', (req, res) => {
-  res.render('view-all-products.ejs');
 });
 
 app.listen(PORT, () => {
