9bf20f6f5f908c81522b2e9f4895f04483aa272e
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-03 15:52:38 -0500

    added 9 more database values

---
+++ db/seeds/02_listings.sql
@@ -0,0 +1,56 @@
+INSERT INTO listings (listing_title, listing_description, image_url, user_id, price, sold_status, created_at)
+
+VALUES ('Art Deco Dining Chair', 'Special limited edition handmade chair.','https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 7944, FALSE, DATE()),
+
+('Floating Cabinet','6 cubby spacious floating cabinet to get extra usable space around the room','https://images.pexels.com/photos/9646747/pexels-photo-9646747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 21999, FALSE, DATE()),
+
+('A pretty cool painting', 'Young up and coming painters new creation', 'https://images.pexels.com/photos/1067556/pexels-photo-1067556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 4000, FALSE, DATE()),
+
+('2018 Hyundai car', 'Only 40000km, family used car for sale','https://images.pexels.com/photos/12163946/pexels-photo-12163946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 20004000, FALSE, DATE()),
+
+('PS5 Controller', 'Brand new red controller','https://images.pexels.com/photos/11591876/pexels-photo-11591876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 6990, FALSE, DATE()),
+
+('Gently used succulent', 'Barely used succulent available for sale', 'https://images.pexels.com/photos/3101767/pexels-photo-3101767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 650, FALSE, DATE()),
+
+('Cacti Trio', 'Great household piece for nature lovers','https://images.pexels.com/photos/5720724/pexels-photo-5720724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 2000, FALSE, DATE()),
+
+('Tennis Racket', 'Gently used racket, comes with 3 tennis balls for free!','https://images.pexels.com/photos/13789945/pexels-photo-13789945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 3035, FALSE, DATE()),
+
+('Plectric piano','Electric Piano, newly cleaned, slightly used and sounds amazing!', 350000, FALSE, DATE()),
+
+
+('Set of tarot cards', 'These cards can be used to read your fortune. Use at your own risk!',
+'https://images.pexels.com/photos/5465791/pexels-photo-5465791.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 549, FALSE, DATE()),
+
+('Black Wallet', 'This wallet can hold all of your debit and credit cards.',
+'https://images.pexels.com/photos/12495667/pexels-photo-12495667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 1299, FALSE, DATE()),
+
+('Iphone 13 Pro Max', 'Iphone 13 Pro Max that I have had for less than a year. Small crack in the scree.',
+'https://images.pexels.com/photos/1647978/pexels-photo-1647978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 12000, FALSE, DATE()),
+
+('Vanilla Candle', 'Brand new vanilla scented candles. Order online here or pick-up at our retail location in Kensignton Market',
+'https://images.pexels.com/photos/12579758/pexels-photo-12579758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 1695, FALSE, DATE()),
+
+('Green Motorcycle', 'Rarely used, aqua green Harley Davidson. Only 19,000 kms, stored inside for the winters a generally well take care of',
+'https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 1723400, FALSE, DATE()),
+
+('Lulu Yoga Mat', 'Ultra durable Lulu Lemon yoga mat. Only been used twice',
+'https://images.pexels.com/photos/8436582/pexels-photo-8436582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 3500, FALSE, DATE()),
+
+('Reebok Running Shoes', 'Womens size 8, Reebok running shoes. Will need to meetup for delivery',
+'https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 4299, FALSE, DATE()),
+
+('65" Sony Flatscreen', 'SONY super HD flatscreen tv. 65" smart TV - comes with Netflix and Disney plus already downloaded',
+'https://images.pexels.com/photos/9646752/pexels-photo-9646752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 23000, FALSE, DATE()),
+
+('Playsation', 'Original Playstation 1, no controllers. Comes with 2 games - Crash Bandicoot and Spyro.',
+'https://images.pexels.com/photos/9704415/pexels-photo-9704415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+1, 7500, FALSE, DATE());
