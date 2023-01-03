6d836af10b208faa678f1bfaf367f254aeda4e3f
Ethan <ethan.steip@gmail.com>
2023-01-03 16:11:34 -0500

    cleaned up schema and seed files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, role) VALUES ('Kash', 'Kash@hotmail.com', 'Admin');
+INSERT INTO users (name, email, role) VALUES ('Ethan', 'Ethan@hotmail.com', 'General');
