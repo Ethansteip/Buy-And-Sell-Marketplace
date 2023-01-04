ecb413d7e8ec74f80f3f9aca661d9e96b99f4231
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-04 14:28:53 -0500

    added redirect for user profile, added messages tables and dummy data

---
+++ db/seeds/03_messages.sql
@@ -0,0 +1,8 @@
+INSERT INTO messages (user_id, messages, created_at)
+
+VALUES (1, 'Hello', NOW()),
+(1, 'Hello back', NOW()),
+(1, 'Hello three', NOW()),
+(2, 'Hello', NOW()),
+(2, 'Hello back', NOW()),
+(2, 'Hello three', NOW());
