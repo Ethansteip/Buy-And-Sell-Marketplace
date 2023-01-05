c89c9c72251c232c5d6080df5cc3cddb230dd20e
Ethan <ethan.steip@gmail.com>
2023-01-05 17:01:45 -0500

    continuing to work on routes

--- routes/view-all-products.js
+++ routes/view-all-products.js
@@ -1,9 +1,42 @@
 
 const express = require('express');
 const router  = express.Router();
+const viewAll = require('../db/queries/view-all');
 
 router.get('/', (req, res) => {
-  res.render('view-all-products.ejs');
+
+  viewAll.getAllListings()
+    .then(listings => {
+      console.log(listings);
+      res.render("view-all-products.ejs", {listings});
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+router.get('/highest', (req, res) => {
+
+  viewAll.GetHighestToLowestListings()
+    .then(listings => {
+      console.log(listings);
+      res.render("view-all-products.ejs", {listings});
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+router.get('/lowest', (req, res) => {
+
+  viewAll.GetLowestToHighestListings()
+    .then(listings => {
+      console.log(listings);
+      res.render("view-all-products.ejs", {listings});
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 });
 
 module.exports = router;
