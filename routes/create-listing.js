c0b964fb878ffffb96fa90cf86a64859404aba5e
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-05 19:06:07 -0500

    added create listing functionality POST route

--- routes/create-listing.js
+++ routes/create-listing.js
@@ -1,11 +1,29 @@
 
 const express = require('express');
 const router  = express.Router();
+const listing = require('../db/queries/make-listing')
 
 router.get('/', (req, res) => {
   res.render('create-listing');
 });
 
+
+
+
+router.post('/new', (req, res) => {
+  const data = req.body;
+
+  console.log(data,"data");
+//------------------------------------------------
+  listing.createListing(data)
+    .then(() => {
+      // res.redirect('/')
+      res.redirect('/')
+    })
+    .catch(err => {
+      console.log("insert error",err.message);
+    });
+});
 
 
 
